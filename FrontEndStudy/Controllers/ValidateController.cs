09423037e56742410d7c2801e2495572a1f29173
LiuTianqing <lhard@qq.com>
2016-09-02 16:19:07 +0800

    study jquery validation plug

---
+++ FrontEndStudy/Controllers/ValidateController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FrontEndStudy.Controllers
+{
+    public class ValidateController : Controller
+    {
+        // GET: Validate
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DoPost(string name="",string pwd="")
+        {
+            return Content(name + "---" + pwd);
+        }
+        [HttpPost]
+        public bool CanLogin(string name = "", string pwd = "")
+        {
+            if (name == "lhard" && pwd == "123")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ FrontEndStudy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FrontEndStudy.MvcApplication" Language="C#" %>
